3cd4b37ca83cdff25caf687b7fed3627b20600f8
Stefano Bichicchi <stefano.bichicchi@u2y.io>
2026-09-10 12:13:55 +0200

    refactor: remove unused components and stores
    - Deleted InterestCard, JokeMarquee, ProjectCard, and ThemeToggle components.
    - Removed lang and theme stores as they are no longer needed.
    - Cleaned up layout and page files by removing unnecessary code.
    - Added global CSS styles for a new brutalist theme.
    - Updated project configuration files to remove unused settings.

--- src/lib/stores/lang.ts
+++
@@ -1,4 +0,0 @@
-import type { Locale } from '$lib/translations';
-import { writable } from 'svelte/store';
-
-export const locale = writable<Locale>('it');
--- src/params/lang.ts
+++
@@ -1,3 +0,0 @@
-export const match = (param: string) => {
-	return param === 'en' || param === 'it';
-};
